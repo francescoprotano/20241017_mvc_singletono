0f2bb0cc8ea3f913f09382742f8f530e696cf969
unknown <f.protano@exolab.it>
2024-10-17 11:21:27 +0200

    secondo commit

--- src/mvc/controller/StudenteController.java
+++ src/mvc/controller/StudenteController.java
@@ -1,5 +1,96 @@
 package mvc.controller;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import mvc.view.StudenteView;
+import no_mvc.Studente;
+
 public class StudenteController {
+	
+	private List<Studente> dati = new ArrayList<Studente>();
+	
+	public void insert() {
+		
+		Studente model = new Studente();
+		StudenteView view = new StudenteView();
+		view.insert(model);
+		
+		dati.add(model);
+		
+		
+		view.print(model);
+		
+		
+		
+	}
+	public void findAll() {
+		
+		StudenteView view = new StudenteView();
+		view.findAll(dati);
+		
+		
+	}
+	
+	
+	public void findByMatricola() {
+		
+		
+		
+		
+		StudenteView view = new StudenteView();
+		Studente model = new Studente();
+		view.findByMatricola(model);
+		
+		String matricola = model.getMatricola();
+		
+		Studente studenteTrovato = null;
+		for(Studente s : dati) {
+			if(s.getMatricola().equals(matricola)) {
+				studenteTrovato = s;
+				break;
+			}
+		}
+		
+		
+		if(studenteTrovato!=null) {
+		
+			view.print(studenteTrovato);
+		} else {
+			view.printNotFound();
+		}
+		
+	}
+ public void findByMatricola2() {
+		
+		
+		
+		
+		StudenteView view = new StudenteView();
+		String matricola = view.findByMatricola2();
+		
+		
+		
+		Studente studenteTrovato = null;
+		for(Studente s : dati) {
+			if(s.getMatricola().equals(matricola)) {
+				studenteTrovato = s;
+				break;
+			}
+		}
+		
+		
+		if(studenteTrovato!=null) {
+		
+			view.print(studenteTrovato);
+		} else {
+			view.printNotFound();
+		}
+		
+	}
+
 
 }
--- src/mvc/view/StudenteView.java
+++ src/mvc/view/StudenteView.java
@@ -1,5 +1,78 @@
 package mvc.view;
 
+import java.util.List;
+import java.util.Scanner;
+
+import no_mvc.Studente;
+
 public class StudenteView {
+	
+	public void insert(Studente model) {
+
+		Scanner input = new Scanner(System.in);//questa finisce nella view
+		
+		System.out.println("Digita matricola:");//anche questa finisce nella view
+		String matricola = input.nextLine();//anche questa finisce nella view
+		
+		System.out.println("Digita nome:");//anche questa finisce nella view
+		String nome = input.nextLine();//anche questa finisce nella view
+		
+		System.out.println("Digita cognome:");//anche questa finisce nella view
+		String cognome = input.nextLine();//anche questa finisce nella view
+		
+		
+		model.setCognome(cognome);
+		model.setMatricola(matricola);
+		model.setNome(cognome);
+		
+		
+		
+		
+		input.close();//anche questa finisce nella view
+	}
+	
+	public void print(Studente model) {
+		
+		System.out.println(model.getCognome() + ", " + model.getNome());
+		
+	}
+	
+	public void findAll(List<Studente> dati) {
+		
+		for(Studente model : dati) {
+			print(model);
+			
+		}
+		
+		
+	}
+	
+	
+	public void findByMatricola(Studente model) {
+		Scanner input = new Scanner(System.in);//questa finisce nella view
+		String matricola = input.nextLine();//questa finisce nella view
+		model.setMatricola(matricola);
+		
+		input.close();//questa finisce nella view
+		
+	}
+	
+	public String findByMatricola2() {
+		Scanner input = new Scanner(System.in);//questa finisce nella view
+		String matricola = input.nextLine();//questa finisce nella view
+		
+		input.close();//questa finisce nella view
+		
+		return matricola;
+		
+	}
+	
+	
+	public void printNotFound() {
+		System.out.println("non trovato");
+		
+	}
+
+
 
 }
